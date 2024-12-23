92061ccfaaeffcfe9bd9207aaae313bf507bbb4d
Kaitlyn <kaitlynpeters@cox.net>
2024-12-23 17:09:54 -0600

    files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export',
+    basePath: '/resume-website',
+    trailingSlash: true,
+    images: {
+        unoptimized: true, // Disable image optimization
+      },
+  };
+  
 export default nextConfig;
